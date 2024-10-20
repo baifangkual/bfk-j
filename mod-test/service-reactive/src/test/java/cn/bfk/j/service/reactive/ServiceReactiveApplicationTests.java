74f8a35a7ceffbff3e6fd8975bc8db4ad5e5e016
baifangkual <baifangkuai@qq.com>
2024-10-20 20:42:40 +0800

    add * 添加基本公共和测试模块版本管理等

---
+++ mod-test/service-reactive/src/test/java/cn/bfk/j/service/reactive/ServiceReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.bfk.j.service.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceReactiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
