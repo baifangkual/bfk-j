c7d484ea4b34da4fc59b43b940d0cb75bd599bcc
baifangkual <baifangkuai@live.com>
2025-05-10 19:40:00 +0800

    mod * 修改完整Cfg类

---
+++ mod-core/src/main/java/io/github/baifangkual/bfk/j/mod/core/conf/CfgOptionKeyDuplicateException.java
@@ -0,0 +1,33 @@
+package io.github.baifangkual.bfk.j.mod.core.conf;
+
+import io.github.baifangkual.bfk.j.mod.core.fmt.STF;
+
+/**
+ * 表示在一个配置类实例中设置{@link Cfg.Option#key()}重复的配置项
+ *
+ * @author baifangkual
+ * @since 2025/5/10
+ */
+class CfgOptionKeyDuplicateException extends IllegalArgumentException {
+    CfgOptionKeyDuplicateException(String optionKey) {
+        super(buildErrMsgByOptionKey(optionKey));
+    }
+
+    CfgOptionKeyDuplicateException(Cfg.Option<?> option) {
+        super(buildErrMsgByOption(option));
+    }
+
+    /**
+     * 重复配置的异常信息模板
+     */
+    private static final String EXISTS_MSG_TEMP = "Cfg.Option({}) already exists in Cfg instance";
+
+
+    static String buildErrMsgByOption(Cfg.Option<?> option) {
+        return buildErrMsgByOptionKey(option.key());
+    }
+
+    static String buildErrMsgByOptionKey(String optionKey) {
+        return STF.f(EXISTS_MSG_TEMP, optionKey);
+    }
+}
---
+++ mod-core/src/test/java/io/github/baifangkual/bfk/j/mod/core/conf/CfgTest.java
@@ -0,0 +1,144 @@
+package io.github.baifangkual.bfk.j.mod.core.conf;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+/**
+ * @author baifangkual
+ * @since 2025/5/5
+ */
+public class CfgTest {
+
+
+    @Test
+    public void test01() {
+
+        Cfg cfg = Cfg.ofNew();
+
+        Cfg.Option<Boolean> opt1 = Cfg.Option.of("opt1")
+                .booleanType()
+                .defaultValue(true)
+                .build();
+
+        cfg.set(opt1, true);
+        Assertions.assertEquals(true, cfg.unsafeGet(opt1));
+
+    }
+
+    @Test
+    public void test02() {
+        // not found msg test
+        Cfg cfg = Cfg.ofNew();
+
+        Cfg.Option<String> opt = Cfg.Option.of("opt1.req")
+                .stringType()
+                .defaultValue("defaultString")
+                .description("this is opt1.req description")
+                .notFoundValueMsg("this is opt1.req noValueMsg")
+                .build();
+
+        Optional<String> s = cfg.get(opt);
+        Assertions.assertEquals(Optional.empty(), s);
+
+    }
+
+    @SuppressWarnings("CommentedOutCode")
+    @Test
+    public void test03() {
+        // not found msg test
+        Cfg cfg = Cfg.ofNew();
+
+        Cfg.Option<String> opt1FallBack1 = Cfg.Option.of("opt1.fallback1")
+                .stringType()
+                .defaultValue("opt1FallBack1DefaultValue")
+                .notFoundValueMsg("this is opt1.fallback1 noValueMsg")
+                .build();
+
+        Cfg.Option<String> opt1FallBack2 = Cfg.Option.of("opt1.fallback2")
+                .stringType()
+                .description("this is opt1.fallback2 description")
+                .notFoundValueMsg("this is opt1.fallback2 noValueMsg")
+                .build();
+
+        Cfg.Option<String> opt1FallBack3 = Cfg.Option.of("opt1.fallback3")
+                .stringType()
+                .defaultValue("opt1FallBack3DefaultValue")
+                .description("this is opt1.fallback3 description")
+                .build();
+
+        Cfg.Option<String> opt = Cfg.Option.of("opt1.req")
+                .stringType()
+                .defaultValue("defaultString")
+                .description("this is opt1.req description")
+                .notFoundValueMsg("this is opt1.req noValueMsg")
+                .fallbackOf(opt1FallBack1, opt1FallBack2, opt1FallBack3)
+                .build();
+
+//        String s1 = cfg.unsafeGet(opt);
+//        System.out.println(opt);
+//        System.out.println(opt1FallBack1);
+
+        Assertions.assertThrows(CfgOptionValueNotFoundException.class, () -> {
+            String s = cfg.unsafeGet(opt);
+            System.out.println(s);
+        });
+    }
+
+    @Test
+    public void test04() {
+        // get unsafeGet and getOrDefault and unsafeGetOrDefault test
+        Cfg cfg = Cfg.ofNew();
+
+        final String defaultString = "defaultString";
+
+        Cfg.Option<String> strOpt = Cfg.Option.of("cfgOption")
+                .defaultValue(defaultString)
+                .description("this is opt.cfgOption description")
+                .build();
+
+        Optional<String> s = cfg.get(strOpt);
+        Assertions.assertEquals(Optional.empty(), s);
+        Assertions.assertThrows(CfgOptionValueNotFoundException.class, () -> cfg.unsafeGet(strOpt));
+        Optional<String> orDefault = cfg.getOrDefault(strOpt);
+        Assertions.assertEquals(Optional.of(defaultString), orDefault);
+        Assertions.assertEquals(defaultString, cfg.unsafeGetOrDefault(strOpt));
+
+
+    }
+
+    @Test
+    public void test05() {
+        Cfg cfg = Cfg.ofNew();
+        final String defaultString = "defaultString";
+        Cfg.Option<String> strOpt1 = Cfg.Option.of("cfgOption")
+                .stringType()
+                .build();
+
+        Cfg.Option<String> strOpt2 = Cfg.Option.of("cfgOption")
+                .stringType()
+                .build();
+
+        cfg.set(strOpt1, defaultString);
+        Assertions.assertThrows(CfgOptionKeyDuplicateException.class, () -> cfg.set(strOpt2, defaultString));
+
+    }
+
+    @Test
+    public void test06() {
+        // reset test
+        Cfg cfg = Cfg.ofNew();
+        final String defaultString = "defaultString";
+        Cfg.Option<String> strOpt1 = Cfg.Option.of("cfgOption")
+                .stringType()
+                .build();
+
+        Cfg.Option<String> strOpt2 = Cfg.Option.of("cfgOption")
+                .stringType()
+                .build();
+
+        cfg.set(strOpt1, defaultString);
+        cfg.reset(strOpt2, defaultString);
+    }
+}
