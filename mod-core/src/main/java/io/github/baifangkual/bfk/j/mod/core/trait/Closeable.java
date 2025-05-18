34a422d5de5475e8ea5bfa763fa86629250c2677
baifangkual <baifangkuai@live.com>
2025-05-18 15:21:06 +0800

    docs: 丰富 Closeable doc

--- mod-core/src/main/java/io/github/baifangkual/bfk/j/mod/core/trait/Closeable.java
+++ mod-core/src/main/java/io/github/baifangkual/bfk/j/mod/core/trait/Closeable.java
@@ -1,12 +1,12 @@
 package io.github.baifangkual.bfk.j.mod.core.trait;
 
 
-import io.github.baifangkual.bfk.j.mod.core.panic.PanicException;
 import io.github.baifangkual.bfk.j.mod.core.panic.Err;
+import io.github.baifangkual.bfk.j.mod.core.panic.PanicException;
 
 /**
  * <b>可关闭的</b><br>
- * 表示 有状态对象，该对象有是否已关闭的状态，并且能够明确关闭其<br>
+ * 表示 有状态对象，该对象有是否已关闭的状态，并且能够明确关闭其，可使用{@link #isClosed()}查看该是否已被关闭<br>
  * 因为该接口继承了{@link AutoCloseable}，遂仍旧可使用语法糖 try-with-resource<br>
  * 该接口不对实现类的重复调用{@link #close()} 方法做约束<br>
  *
@@ -15,9 +15,9 @@ import io.github.baifangkual.bfk.j.mod.core.panic.Err;
  */
 public interface Closeable extends AutoCloseable {
     /**
-     * 返回该实例是否已关闭
+     * 返回是否已关闭
      *
-     * @return true:已关闭，false：未关闭
+     * @return true 已关闭，反之未关闭
      */
     boolean isClosed();
 
@@ -30,7 +30,8 @@ public interface Closeable extends AutoCloseable {
     void close() throws Exception;
 
     /**
-     * close方法的方便包装，调用方无需在作用域声明预检异常，关闭过程中发生的预检异常将被包装为{@link PanicException}并抛出
+     * 恐慌关闭<br>
+     * 无需在作用域声明预检异常，关闭过程中发生的预检异常将被包装为{@link PanicException}并抛出
      *
      * @see #close()
      */
@@ -43,8 +44,11 @@ public interface Closeable extends AutoCloseable {
     }
 
     /**
-     * close方法的方便包装，调用方无需在作用域声明预检异常，但关闭过程中如果发生异常则还是会抛出相应异常
+     * 偷偷摸摸关闭<br>
+     * 无需在作用域声明预检异常，但关闭过程中如果发生异常则还是会抛出相应异常，即使该异常为预检异常
      *
+     * @apiNote 该方法因为可以抛出预检异常而不在上下文中显式声明要抛出的预检异常，遂从外部/上层方法签名中无法得知可能抛出的
+     * 预检异常，使用该方法要明确该方法可能造成的风险
      * @see #close()
      */
     default void sneakyClose() {
@@ -56,11 +60,12 @@ public interface Closeable extends AutoCloseable {
     }
 
     /**
-     * close方法的方便包装，调用方无需在作用域声明预检异常，关闭过程中发生的异常将被屏蔽
+     * 斯大林关闭<br>
+     * 无需在作用域声明预检异常，关闭过程中发生的异常将被处决
      *
      * @see #close()
      */
-    default void quietlyClose() {
+    default void stalinClose() {
         try {
             close();
         } catch (Exception e) {/* pass */}
