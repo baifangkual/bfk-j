13ce0b3a3d920e6f7879baf83bf477e5cbce5cf8
baifangkual <baifangkuai@live.com>
2025-05-24 01:42:30 +0800

    refactor: id generator
    - IdGen -> Idg

---
+++ mod-core/src/main/java/io/github/baifangkual/bfk/j/mod/core/util/Idg.java
@@ -0,0 +1,290 @@
+package io.github.baifangkual.bfk.j.mod.core.util;
+
+import io.github.baifangkual.bfk.j.mod.core.fmt.STF;
+
+import java.lang.management.ManagementFactory;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * <b>ID Generator</b><br>
+ * 线程安全，生成的ID为雪花ID，共 63 位有效位，符号位永远为 0<br>
+ * 其中 41 位为时间偏移量，{@value MACHINE_ID_BITS} 位为 machineId 标识，{@value CENTER_ID_BITS} 位为 centerId 标识，
+ * {@value SEQUENCE_BITS} 位为同毫秒细分的序列号<br>
+ * 该Id构成: {@code 0(符号位) - 时间偏移量 - 数据中心标识 - 机器码 - 同毫秒序列号}<br>
+ * 该工具类对时间回溯没有太大抗性，当两次通过 {@link #nextId()} 获取id的间隔中发生多次
+ * 或单次大于 {@value #MAX_FAULT_TOLERANT_BACKTRACKING_CAPACITY} 毫秒的时间回溯时，将抛出运行时异常<br>
+ * <p>
+ * 该算法及代码参考：
+ * <a href="https://zh.wikipedia.org/wiki/%E9%9B%AA%E8%8A%B1%E7%AE%97%E6%B3%95">雪花算法</a>
+ * <a href="https://gitee.com/yu120/sequence/blob/master/src/main/java/cn/ms/sequence/Sequence.java">分布式高效ID生产黑科技</a>
+ * <a href="https://github.com/beyondfengyu/SnowFlake">SnowFlake</a>
+ *
+ * @author baifangkual
+ * @since 2024/12/12 v0.0.3
+ */
+public final class Idg {
+
+    private static final Idg INSTANCE = new Idg();
+
+    /**
+     * 返回一个雪花ID
+     *
+     * @return 雪花ID
+     * @throws IllegalStateException 当时间发生至少两次连续回溯,
+     *                               或时间发生一次大于 {@value MAX_FAULT_TOLERANT_BACKTRACKING_CAPACITY} ms 的回溯时
+     * @throws IllegalStateException 当线程收到中断信号时
+     * @apiNote 该方法依赖的底层方法在高并发情况下会频繁进行系统调用
+     * @see Idg#nextId()
+     */
+    public static long longId() {
+        return INSTANCE.nextId();
+    }
+
+    // DEFINITION ==================================
+    /**
+     * 宇宙大爆炸时间<br>
+     * 当前设定值 1609459200000L 表示时间 2021-01-01 00:00:00(UTC/GMT+08:00)<br>
+     * 不能在 {@code System.currentTimeMillis()} 小于该值时使用
+     */
+    static final long EPOCH_BEGIN = 1609459200000L;
+    /**
+     * 宇宙热寂时间<br>
+     * {@value EPOCH_BEGIN} + 2199023255551L = 2090-09-07 23:47:35(UTC/GMT+08:00)<br>
+     * 不能在 {@code System.currentTimeMillis()} 大于该值时使用
+     */
+    static final long EPOCH_END = EPOCH_BEGIN + 2199023255551L;
+    /**
+     * 数据中心区分，占多少bit，数据中心个数最多 {@value MAX_CENTER_ID_VALUE} 个
+     */
+    static final long CENTER_ID_BITS = 5L;
+    /**
+     * 机器码区分，占多少bit，机器最多区分 {@value MAX_MACHINE_ID_VALUE} 个
+     */
+    static final long MACHINE_ID_BITS = 7L;
+    /**
+     * 同毫秒区分-序列码区分，占多少bit，每毫秒内可用的id数: {@value MAX_SEQUENCE_VALUE} 个
+     */
+    static final long SEQUENCE_BITS = 10L;
+    /**
+     * 最大的容忍回溯和闰秒的毫秒数，该值不宜过大，因为对回溯的抵抗将由线程的TIME-SLEEP负责
+     */
+    static final long MAX_FAULT_TOLERANT_BACKTRACKING_CAPACITY = 5L;
+
+    // 最大值
+    static final long MAX_CENTER_ID_VALUE = ~(-1L << CENTER_ID_BITS);
+    static final long MAX_MACHINE_ID_VALUE = ~(-1L << MACHINE_ID_BITS);
+    static final long MAX_SEQUENCE_VALUE = ~(-1L << SEQUENCE_BITS);
+
+    // bit 偏移量
+    static final long CENTER_ID_OFFSET = MACHINE_ID_BITS + SEQUENCE_BITS;
+    static final long TIMESTAMP_OFFSET = CENTER_ID_BITS + MACHINE_ID_BITS + SEQUENCE_BITS;
+
+    // 区分01-数据中心
+    private final long centerId;
+    // 区分02-机器码
+    private final long machineId;
+
+    // MUTABLE =====================================
+    // 序列号原子变量
+    private long sequence = 0;
+    // 上次生产 ID 时间戳, 初始值设定为 -1L
+    private long lastTime = -1L;
+
+    /**
+     * centerId 使用 mac地址后10位余数，machineId 使用当前计算机名+进程pid哈希后16位余数<br>
+     * machineId {@value  #MACHINE_ID_BITS} 位，centerId 应当占 {@value  #CENTER_ID_BITS} 位
+     */
+    public Idg() {
+        this(defaultMachineId(), defaultCenterId());
+    }
+
+    /**
+     * 明确指定 区分 machineId 和 centerId 的构造参数<br>
+     * machineId {@value  #MACHINE_ID_BITS} 位，centerId 应当占 {@value  #CENTER_ID_BITS} 位
+     *
+     * @param machineId 同毫秒区分-机器码
+     * @param centerId  同毫秒区分-数据中心Id
+     */
+    public Idg(long machineId, long centerId) {
+        long nowTimeMillis = nowSystemTime();
+        if (nowTimeMillis < EPOCH_BEGIN || nowTimeMillis > EPOCH_END) {
+            LocalDateTime now = LocalDateTime
+                    .ofInstant(Instant.ofEpochMilli(nowTimeMillis), ZoneId.systemDefault());
+            LocalDateTime bigBang = LocalDateTime
+                    .ofInstant(Instant.ofEpochMilli(EPOCH_BEGIN), ZoneId.systemDefault());
+            LocalDateTime deathOfTime = LocalDateTime
+                    .ofInstant(Instant.ofEpochMilli(EPOCH_END), ZoneId.systemDefault());
+            DateTimeFormatter dft = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss:SSS");
+            throw new IllegalStateException(STF.f("ID生成器初始化失败，不允许当前系统时间 '{}' 早于 '{}' 或晚于 '{}'",
+                    now.format(dft), bigBang.format(dft), deathOfTime.format(dft)));
+        }
+        if (machineId > MAX_MACHINE_ID_VALUE || machineId < 0) {
+            throw new IllegalArgumentException(STF.f("machineId > {} or < 0", MAX_MACHINE_ID_VALUE));
+        }
+        if (centerId > MAX_CENTER_ID_VALUE || centerId < 0) {
+            throw new IllegalArgumentException(STF.f("centerId > {} or < 0", MAX_CENTER_ID_VALUE));
+        }
+        this.machineId = machineId;
+        this.centerId = centerId;
+    }
+
+    /**
+     * 生成雪花ID<br>
+     * 在同一毫秒内最多可生成 {@value #MAX_SEQUENCE_VALUE} 个雪花Id，
+     * 若一个毫秒内请求数大于 {@value #MAX_SEQUENCE_VALUE} 则从第 {@value #MAX_SEQUENCE_VALUE} +1 个请求开始，
+     * 请求所在的线程将会忙自旋到下一毫秒再从该方法返回雪花ID
+     *
+     * @return 雪花ID
+     * @throws IllegalStateException 当时间发生至少两次连续回溯,
+     *                               或时间发生一次大于 {@value #MAX_FAULT_TOLERANT_BACKTRACKING_CAPACITY} ms 的回溯时
+     * @throws RuntimeException      当线程收到中断信号时
+     * @apiNote 该方法依赖的底层方法在高并发情况下会频繁进行系统调用
+     */
+    public synchronized long nextId() {
+        long timestamp = nowSystemTime();
+        if (timestamp < lastTime) {  // 可能闰秒，可能发生大回溯
+            long offset = lastTime - timestamp;
+            if (offset <= MAX_FAULT_TOLERANT_BACKTRACKING_CAPACITY) {  // 差值小于设定可容忍值，等待
+                try {
+                    TimeUnit.MILLISECONDS.sleep(offset << 1);  // 尝试休眠双倍差值后重新获取，再次校验
+                    timestamp = nowSystemTime();
+                    if (timestamp < lastTime) {  // 等待结束后，如果新获取的仍小于，则证明时间又发生回溯
+                        throw new IllegalStateException(STF
+                                .f("系统时间发生回溯，拒绝在 {} 毫秒内生成Id", lastTime - timestamp));
+                    }
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt(); // 恢复中断信号，闰秒或极小回溯的线程休眠情况几乎不可能发生或被观测到
+                    throw new IllegalStateException(e);
+                }
+            } else { // 大的回溯，直接他妈异常
+                throw new IllegalStateException(STF.f("系统时间发生回溯，拒绝在 {} 毫秒内生成Id", offset));
+            }
+        }
+
+        if (lastTime == timestamp) {
+            // 相同毫秒内，序列号自增
+            sequence = (sequence + 1) & MAX_SEQUENCE_VALUE;
+            if (sequence == 0L) {
+                // 当进入该块，则表示毫秒数相同且序列号已达到最大, 线程则在该方法内忙自旋到至少下一毫秒才返回
+                timestamp = loop2NextTime(lastTime);
+            }
+        } else {
+            sequence = 0L;
+        }
+
+        lastTime = timestamp;
+        return buildId(timestamp, centerId, machineId, sequence);
+    }
+
+    private static long buildId(long timeMillis, long centerId, long machineId, long sequence) {
+        // 时间戳部分 | 数据中心部分 | 机器标识部分 | 序列号部分
+        return ((timeMillis - EPOCH_BEGIN) << TIMESTAMP_OFFSET)
+               | (centerId << CENTER_ID_OFFSET)
+               | (machineId << SEQUENCE_BITS)
+               | sequence;
+    }
+
+    /**
+     * 重置序列码并自旋，方法内当前线程会忙自旋到至少下一毫秒再返回<br>
+     *
+     * @return 新时间
+     * @see #nowSystemTime()
+     */
+    private long loop2NextTime(long lastTimestamp) {
+        long timestamp = nowSystemTime();
+        while (timestamp <= lastTimestamp) { // 这里，一个毫秒内，线程将会忙到下一个毫秒才会返回
+            timestamp = nowSystemTime();
+        }
+        return timestamp;
+    }
+
+    /**
+     * 获取系统时间<br>
+     *
+     * @return 系统时间
+     * @apiNote 该方法在高并发情况下会频繁进行系统调用，
+     * 遂该方法在高并发情况下使用并不好，高并发情况下或应使用ScheduledExecutorService缓存时间戳值
+     */
+    private long nowSystemTime() {
+        /*
+        原有这里使用 ScheduledExecutorService 在高并发情境下优化 System.currentTimeMillis 的性能问题，这里省去了该过程
+         */
+        return System.currentTimeMillis();
+    }
+
+    /**
+     * 通过mac地址后10位构建 centerId<br>
+     * 占用 {@value #CENTER_ID_BITS} 位
+     *
+     * @return centerId
+     * @apiNote 当无网卡时，返回 {@code 1}，
+     * 当获取网卡地址失败时，返回 {@value #MAX_CENTER_ID_VALUE}
+     */
+    private static long defaultCenterId() {
+        long id = 0L;
+        try {
+            // 拿个网卡MAC地址后两字节值
+            InetAddress address = InetAddress.getLocalHost();
+            NetworkInterface network = NetworkInterface.getByInetAddress(address);
+            if (null == network) {
+                id = 1L; // 无网卡
+            } else {
+                byte[] mac = network.getHardwareAddress();
+                if (null != mac) {
+                    id = ((0x00FFL & (long) mac[mac.length - 2]) | (0xFF00L & (((long) mac[mac.length - 1]) << 8))) >> 6;
+                    id = id % (MAX_CENTER_ID_VALUE + 1);
+                }
+            }
+        } catch (Exception e) {
+            //throw new IllegalStateException(e.getMessage(), e);
+            id = MAX_CENTER_ID_VALUE; // 不抛出异常, 直接给最大值
+        }
+        return id;
+    }
+
+    /**
+     * 通过进程号和电脑名构建 machineId<br>
+     * 占用 {@value #MACHINE_ID_BITS} 位
+     *
+     * @return machineId
+     */
+    private static long defaultMachineId() {
+        // 获取 pid+电脑名
+        String pidAndCptName = ManagementFactory.getRuntimeMXBean().getName();
+        return (pidAndCptName.hashCode() & 0xffff) % (MAX_MACHINE_ID_VALUE + 1);
+    }
+
+    /**
+     * {@link Idg}生成的Id结构记录
+     *
+     * @param genTimeMillis 生成时间
+     * @param centerId      数据中心Id
+     * @param machineId     机器码
+     * @param sequence      序列号
+     */
+    public record Id(long genTimeMillis, long centerId, long machineId, long sequence) {
+        /**
+         * 接收一个{@link Idg#nextId()}生成的Id，返回该Id的结构记录
+         *
+         * @param snowflakeId 雪花Id
+         * @return Id结构记录
+         */
+        public static Id ofLongId(long snowflakeId) {
+            long genTimeMillis = (snowflakeId >> TIMESTAMP_OFFSET) + EPOCH_BEGIN;
+            long centerId = (~(-1 << CENTER_ID_BITS)) & (snowflakeId >> CENTER_ID_OFFSET);
+            long machineId = (~(-1 << MACHINE_ID_BITS)) & (snowflakeId >> SEQUENCE_BITS);
+            long sequence = (~(-1 << SEQUENCE_BITS)) & snowflakeId;
+            return new Id(genTimeMillis, centerId, machineId, sequence);
+        }
+
+        public long toLongId() {
+            return buildId(genTimeMillis, centerId, machineId, sequence);
+        }
+    }
+}
---
+++ mod-core/src/test/java/io/github/baifangkual/bfk/j/mod/core/util/IdgTest.java
@@ -0,0 +1,197 @@
+package io.github.baifangkual.bfk.j.mod.core.util;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.List;
+
+/**
+ * @author baifangkual
+ * @since 2025/5/22
+ */
+public class IdgTest {
+    @Test
+    public void test1() {
+        ZoneId zoneId = ZoneId.systemDefault();
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime sTime = LocalDateTime
+                .ofInstant(Instant.ofEpochMilli(Idg.EPOCH_BEGIN), zoneId);
+        Assertions.assertTrue(sTime.isBefore(now));
+        LocalDateTime eTime = LocalDateTime
+                .ofInstant(Instant.ofEpochMilli(Idg.EPOCH_BEGIN + 2199023255551L), zoneId);
+        LocalDateTime eTime2 = LocalDateTime
+                .ofInstant(Instant.ofEpochMilli(Idg.EPOCH_BEGIN + 4398046511103L), zoneId);
+        Assertions.assertTrue(eTime.isAfter(now));
+        //System.out.println(eTime2);
+    }
+
+    @Test
+    public void test3() {
+        ZoneId zoneId = ZoneId.systemDefault();
+        for (int i = 0; i < 100; i++) {
+            long id = Idg.longId();
+            Idg.Id idObj = Idg.Id.ofLongId(id);
+            //System.out.println(idObj);
+            LocalDateTime genTime = LocalDateTime.ofInstant(Instant.ofEpochMilli(idObj.genTimeMillis()), zoneId);
+            Assertions.assertTrue(genTime.isBefore(LocalDateTime.now()));
+        }
+    }
+
+    record ShowBox(IdR idr, String startTime, String endTime, int threadNum) {
+    }
+
+    record IdR(long timeOffset, long centerId, long machineId, long sequence) {
+        static IdR ofSId(long sId) {
+            long sequence = (~(-1 << 10)) & sId;
+            long machineId = (~(-1 << 7)) & (sId >> 10);
+            long centerId = (~(-1 << 5)) & (sId >> 17);
+            long timeOffset = sId >> 22;
+            return new IdR(timeOffset, centerId, machineId, sequence);
+        }
+    }
+
+    record AsyncR(long startMils, long endMils, List<Long> genIds, int threadNum) {
+    }
+
+
+//    @Test
+//    public void test3() {
+//
+//        // ======== test time=========
+//        final int testTimeMils = 1 * 1000;
+//        final int collectorResultSize = 10000;
+//
+//        SystemClockT.INSTANCE.initialize(); // init
+//        int cupNum = Runtime.getRuntime().availableProcessors();
+//        AtomicInteger cup = new AtomicInteger(0);
+//        ZoneId zoneId = ZoneId.systemDefault();
+//        System.out.println("cupNum = " + cupNum);
+//        System.out.println("testTimeSeconds = " + testTimeMils / 1000);
+//        System.out.println("start!");
+//        List<CompletableFuture<AsyncR>> allFutures = new ArrayList<>();
+//
+//        for (int i = 0; i < cupNum; i++) {
+//            CompletableFuture<AsyncR> asyncR = CompletableFuture.supplyAsync(() -> {
+//                final int threadNum = cup.getAndIncrement();
+//                final SystemClockT clock = SystemClockT.INSTANCE;
+//                // collector result 不在线程内使用阻塞的system.out
+//                final List<Long> genIds = new ArrayList<>(collectorResultSize);
+//                // start ---
+//                long startMils = clock.currentTimeMillis();
+//                while (clock.currentTimeMillis() - startMils < testTimeMils) {
+//                    long l = Roll.nextId();
+//                    genIds.add(l);
+//                }
+//                long endMils = clock.currentTimeMillis();
+//                return new AsyncR(startMils, endMils, genIds, threadNum);
+//            });
+//            allFutures.add(asyncR);
+//        }
+//
+//        // 主线程等待子都结束
+//        CompletableFuture.allOf(allFutures.toArray(new CompletableFuture[0])).join();
+//        // 处理
+//        System.out.println("allFutures end! process result!");
+//        final Comparator<IdR> fnSortIdR = Comparator
+//                .comparingLong(IdR::timeOffset)
+//                // eq no need comp...
+////                .thenComparingLong(IdR::centerId)
+////                .thenComparingLong(IdR::machineId)
+//                .thenComparingLong(IdR::sequence);
+//
+//        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss:SSS");
+//        List<ShowBox> allResults = new LinkedList<>();
+//
+//        for (CompletableFuture<AsyncR> f : allFutures) {
+//            AsyncR threadR = f.join();
+//            long startMils = threadR.startMils;
+//            long endMils = threadR.endMils;
+//            LocalDateTime start = LocalDateTime.ofInstant(Instant.ofEpochMilli(startMils), zoneId);
+//            LocalDateTime end = LocalDateTime.ofInstant(Instant.ofEpochMilli(endMils), zoneId);
+//            String strStart = start.format(dtf);
+//            String strEnd = end.format(dtf);
+//
+//            // 都装好收集
+//            threadR.genIds.stream()
+//                    .map(IdR::ofSId)
+//                    .map(idr -> new ShowBox(idr, strStart, strEnd, threadR.threadNum))
+//                    .forEach(allResults::add);
+//        }
+//
+//
+//        List<ShowBox> sorts = new LinkedList<>();
+//        allResults.stream()
+//                .sorted((sb1, sb2) -> fnSortIdR.compare(sb1.idr, sb2.idr))
+//                .forEach(sorts::add);
+//        System.out.println("result count = " + allResults.size());
+//
+//        allResults.clear();
+//        System.gc(); // help gc
+//
+//        AtomicInteger counter = new AtomicInteger(0);
+//        // 5秒约130多万次 太多阻塞System.out 不用这种
+////        for (ShowBox box : sorts) {
+////            System.out.println(STF
+////                    .f("r[{}] t:{}, s:{}, e:{}, id: {}",
+////                            counter.incrementAndGet(), box.threadNum, box.startTime, box.endTime, box.idr));
+////        }
+//
+//
+//        Map<Integer, AtomicInteger> threadLoopTimes = new HashMap<>();
+//        IntStream.range(0, cupNum).forEach(i -> threadLoopTimes.put(i, new AtomicInteger(0)));
+//        AtomicInteger allThreadLoopTimes = new AtomicInteger(0);
+//        ShowBox befBox = null;
+//        for (ShowBox curBox : sorts) {
+//            IdR cur = curBox.idr;
+//            if (befBox != null) {
+//                if (befBox.idr.timeOffset != cur.timeOffset && cur.sequence == 0) {
+//                    threadLoopTimes.get(curBox.threadNum).incrementAndGet();
+
+    /// /                    System.out.println(STF
+    /// /                            .f("bef: {}", befBox));
+    /// /                    System.out.println(STF
+    /// /                            .f("cur: {}", curBox));
+//                }
+//            }
+//
+//            if (cur.sequence == 0) {
+//                allThreadLoopTimes.getAndIncrement();
+//            }
+//
+//            befBox = curBox;
+//        }
+//        System.out.println("allThreadLoopTimes count = " + allThreadLoopTimes.get());
+//        threadLoopTimes.forEach((key, value) -> System.out.println(STF
+//                .f("t: {}, LoopTimes count: {}", key, value.get())));
+//
+//        // 看看是否冲突
+//        int countSize = sorts.size();
+//        Set<IdR> collect = sorts.stream().map(ShowBox::idr)
+//                .collect(Collectors.toSet());
+//        System.out.println(STF.
+//                f("bef去重: {}, after:{}", countSize, collect.size()));
+//        System.out.println(STF.f("no重复: {}", countSize == collect.size()));
+//        Assertions.assertEquals(countSize, collect.size());
+
+    // 共获取到 / loop次
+    // 10s:
+    // 修改重置序号前
+    // 2033663/1986 1313791/1283 1314815/1284 3876412/3784 3235932/3160 1978368/1932
+    // 3910624/3818 2656172/2593
+    // 修改重置序号后
+    // 2603010/2543 2334723/2281 3278850/3203 3256324/3181 2752517/2689 3286020/3210
+    // 20s:
+    // 修改重置序号前
+    // 7618936/7437 4427720/4322 5751358/5613
+    // 修改重置序号后
+    // 6288389/6142 3817474/3729 3873796/3784 8190984/8000
+    // 1s:
+    // 修改重置序号前
+    // 129023/126 130047/127 131278/128 723834/706 197632/193 197631/193 | 2005180/1957 1996668/1948 2011180/1962
+    // 修改重置序号后
+    // 196419/193 129923/128 129923/128 128900/127 128900/127 509456/499 | 441940/433 1972349/1929 1975420/1932
+//    }
+}
